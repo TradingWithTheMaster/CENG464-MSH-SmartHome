6b9be94d94c4250d79df016f45f04edf988f20d5
Amine79x <aminewachich@proton.me>
2025-12-07 15:35:46 +0300

    Update SecurityManager.h

--- include/SecurityManager.h
+++ include/SecurityManager.h
@@ -1,18 +1,23 @@
 #ifndef SECURITY_MANAGER_H
 #define SECURITY_MANAGER_H
 
-#include <vector>
+#include "StateManager.h"
+#include "LogManager.h"
 
 class SecurityManager {
 public:
-    SecurityManager();
+    SecurityManager(StateManager* stateManager = nullptr, LogManager* logger = nullptr);
 
     void armSystem();
     void disarmSystem();
     bool isArmed() const;
 
+    void triggerAlarm();   
+    void callPolice();    
 private:
     bool armed;
+    StateManager* stateManager;
+    LogManager* logManager;
 };
 
 #endif
