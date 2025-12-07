ca58366c251d5e539f812fdae1b1ee1125670a42
Amine79x <aminewachich@proton.me>
2025-12-07 15:37:36 +0300

    Update SecurityManager.cpp

--- src/SecurityManager.cpp
+++ src/SecurityManager.cpp
@@ -1,15 +1,33 @@
 #include "SecurityManager.h"
 
-SecurityManager::SecurityManager() : armed(false) {}
+SecurityManager::SecurityManager(StateManager* stateManager, LogManager* logger)
+    : armed(false), stateManager(stateManager), logManager(logger) {}
 
 void SecurityManager::armSystem() {
     armed = true;
+    if (logManager)
+        logManager->log("Security System Armed");
 }
 
 void SecurityManager::disarmSystem() {
     armed = false;
+    if (logManager)
+        logManager->log("Security System Disarmed");
 }
 
 bool SecurityManager::isArmed() const {
     return armed;
+}
+
+void SecurityManager::triggerAlarm() {
+    if (logManager)
+        logManager->log("ALARM TRIGGERED!");
+
+    if (stateManager)
+        stateManager->setState(SystemState::ALERT);
+}
+
+void SecurityManager::callPolice() {
+    if (logManager)
+        logManager->log("Police have been notified!");
 }
